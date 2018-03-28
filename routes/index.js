8caa47bdbc2efadf81f5d9099c4cd4b2670a612a
Server <november14giel@gmail.com>
2018-03-28 19:17:43 -0400

    handle bar loops through db and renders

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function(err, prod){
+    if (err) throw err;
+    var productChunks = [];
+    var chunksPerRow = 3;//just to say, we want each row to contain three cards/container
+    //here we loop through prod
+    for (var i = 0; i < prod.length; i += chunksPerRow) {// i is incremented by three
+      productChunks.push(prod.slice(i, i + chunksPerRow)) // logic for keeping each row at three chunks
+    }
+    res.render("shop/index", {
+      title: "Shopping Cart",
+      products: productChunks
+    });
+  });
 });
 
 module.exports = router;
